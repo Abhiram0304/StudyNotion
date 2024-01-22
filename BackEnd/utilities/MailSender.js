93588415eb56ce19bab3808a1212650a6c97f756
Abhiram0304 <tanneriabhi@gmail.com>
2024-01-22 21:23:22 +0530

    check - 1
    Checking mail service

--- BackEnd/utilities/MailSender.js
+++ BackEnd/utilities/MailSender.js
@@ -2,6 +2,12 @@ const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 const SendEmail = async (email,title,body) => {
+    console.log("Email :",email);
+    console.log("title :",title);
+    console.log("body : ",body);
+    console.log("MAIL HOST :",process.env.MAIL_HOST);
+    console.log("MAIL USER :",process.env.MAIL_USER);
+    console.log("MAIL PASS :",process.env.MAIL_PASS);
     try{
         const transporter = nodemailer.createTransport({
             service:process.env.MAIL_HOST,
